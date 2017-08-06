aae8fb78acf66cf7e0bb3b410ccf12b6a22087c0
Ryann Al-lahiq <ryann.allahiq@gmail.com>
2017-08-06 15:56:50 +0100

    updated completed test

--- assets/js/custom1.js
+++ assets/js/custom1.js
@@ -1,1 +1,22 @@
 console.log('script from cutsom1');
+    
+    var $item = $('div.item'), //Cache your DOM selector
+        visible = 2, //Set the number of items that will be visible
+        index = 0, //Starting index
+        endIndex = ( $item.length / visible ) - 3; //End index
+    
+    $('div#arrowR').on( "click", function(){
+        if(index < endIndex ){
+          index++;
+          $item.animate({'left':'-=300px'});
+        }
+    });
+    
+    $('div#arrowL').on( "click", function(){
+        if(index > 0){
+          index--;            
+          $item.animate({'left':'+=300px'});
+        }
+    });
+
+ 
